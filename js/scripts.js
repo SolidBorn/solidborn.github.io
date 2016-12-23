ce7a507a11c17034dafb8c24ef0e3a7a2a6d2565
Oliver <olivergeller120@gmail.com>
2016-12-23 00:22:58 -0500

    hacking in progress...

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+///<reference path="/alt1lib.js">
+///<reference path="/runeappslib.js">
+///<reference path="/imagedetect.js">
+///<reference path="/apps/alt1/alt1doc.js">
+
+
+function start() {
+	a1lib.identifyUrl("appconfig.json");
+}

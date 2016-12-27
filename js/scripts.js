86e56d782a52a44a6533c7de3f29761cdb24472d
SolidBorn <loyalmodding@gmail.com>
2016-12-26 23:06:26 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,4 @@
 
 function start() {
 	a1lib.identifyUrl("appconfig.json");
-	var wh = alt1.rsHeight;
-        var ww = alt1.rsWidth;
-        var textSize = 16
-        var timeToShow = 60000;
-        var x = 20;//(wh - textSize) - (textSize+20);
-        var y = parseInt((ww/2) - ((text.length*textSize)/3.15));
-
-        var color = 0000
-            alt1.overLayText('Solid Born testing some text on screen because he wants too', parseInt('0xFF' + color), textSize, y, x, timeToShow);
 }
